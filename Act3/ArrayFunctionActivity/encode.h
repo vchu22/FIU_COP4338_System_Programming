390cc18754eb349d10cd55de298ae952e08fa813
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2021-04-07 23:49:35 -0400

    finish program 4

--- Act3/ArrayFunctionActivity/encode.h
+++ Act3/ArrayFunctionActivity/encode.h
@@ -2,5 +2,6 @@
 #define ENCODE_H
 
 void encode(char *input, int shift);
+void decode(char *input, int shift);
 
 #endif
--- Act3/ArrayFunctionActivity/encodecode.c
+++ Act3/ArrayFunctionActivity/encodecode.c
@@ -8,3 +8,12 @@ void encode(char input[80], int shift)
       input[i] = input[i] + shift;
   }
 }
+
+void decode(char *input, int shift)
+{
+  for (int i = 0; i < strlen(input); i++)
+  {
+    if (!isspace(input[i]))
+      input[i] = input[i] - shift;
+  }
+}
