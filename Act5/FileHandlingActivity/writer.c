7ebeede283d8d4b7ef2d5a439ecb173371657c3e
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2021-04-13 16:58:39 -0400

    finish FileHandlingActivity program 1

--- Act5/FileHandlingActivity/writer.c
+++ Act5/FileHandlingActivity/writer.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void StringToLower(char str[]){
+  for (int i=0; i<strlen(str); i++) {
+    str[i] = tolower(str[i]);
+  }
+}
+void StringToUpper(char str[]){
+  for (int i=0; i<strlen(str); i++) {
+    str[i] = toupper(str[i]);
+  }
+}
 
 int main(int argc, char *argv[]) 
 {
@@ -39,10 +52,57 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
+  if ((argc - optind) < 3 || (argc - optind) > 10) {
+    printf("Argument string must between 3 to 10 words.\n");
+    return -1;
+  }
+  char argString[80];
   if (fflag) {
-    // create a file with that name or overwrite a previously existing file and write your name in on the first line followed by the "argument string". 
+    // create a file with that name or overwrite a previously existing file and write your name in on the first line followed by the "argument string".
+    FILE* outFile = NULL;
+    outFile = fopen(fname, "w");
+    if (outFile == NULL) {
+      printf("Could not open file %s.\n", fname);
+      return -1;
+    }
+    if (optind < argc) /* these are the arguments after the command-line options */
+    {
+      for (; optind < argc; optind++) {
+        if (lflag){
+          StringToLower(argv[optind]);
+          fprintf(outFile, "%s ", argv[optind]);
+        }
+        else if (uflag){
+          StringToUpper(argv[optind]);
+          fprintf(outFile, "%s ", argv[optind]);
+        }
+        else
+          fprintf(outFile, "%s ", argv[optind]);
+      }
+      fprintf(outFile, "\n");
+      fflush(stdout);
+    }
+    fclose(outFile);
   } else {
     // only write your name to the screen followed by "argument string".
+    if (optind < argc) /* these are the arguments after the command-line options */
+    {
+      for (; optind < argc; optind++) {
+        if (lflag){
+          StringToLower(argv[optind]);
+          fprintf(stdout, "%s ", argv[optind]);
+        }
+        else if (uflag){
+          StringToUpper(argv[optind]);
+          fprintf(stdout, "%s ", argv[optind]);
+        }
+        else
+          fprintf(stdout, "%s ", argv[optind]);
+      }
+      fprintf(stdout, "\n");
+      fflush(stdout);
+    }
   }
+  
   return 0;
 }
