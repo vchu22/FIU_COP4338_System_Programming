5be6aea2ddca54b9dcc4a339ea63ea471951d6ea
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2021-04-14 00:06:38 -0400

    save progress

--- Act5/PointerMemlocStructActivity/main.c
+++ Act5/PointerMemlocStructActivity/main.c
@@ -7,16 +7,20 @@ int main(void)
   Structure **stc = (Structure *)malloc(sizeof(Structure) * 5);
 
   // Ask the user to enter information for each of these variables one at a time.
-  // GetInputs(stc1);
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < 1; i++)
+  {
+    GetInputs(&stc + sizeof(Structure) * i);
+  }
+  printf("\n");
+  for (int i = 0; i < 1; i++)
   {
     printf("Structure %d:\n", i + 1);
     printf("\tStructure %d pointer: %p\n", i + 1, &stc + sizeof(Structure) * i);
-    printf("\tCharacter:\n");
+    printf("\tCharacter: %c\n", stc[0]->character);
     printf("\tInteger:\n");
     printf("\tString:\n");
     printf("\tFloating Point:\n");
   }
-
+  free(stc);
   return 0;
 }
--- Act5/PointerMemlocStructActivity/structure.c
+++ Act5/PointerMemlocStructActivity/structure.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "structure.h"
 
 void GetInputs(Structure *stc)
 {
+  char c;
+  int i;
+  char s[50];
+  float f;
+
   fflush(stdin);
   printf("Enter a single character:\n");
-  scanf("%c", (*stc).character);
+  scanf("%c", &c);
   printf("Enter an integer:\n");
-  scanf("%i", (*stc).integer);
+  scanf("%i", &i);
   fflush(stdin);
   printf("Enter a phrase:\n");
-  fgets((*stc).stringp, 50, stdin);
-  if ((*stc).stringp[strlen((*stc).stringp) - 1] == '\n')
-    (*stc).stringp[strlen((*stc).stringp) - 1] = '\0';
+  fgets(s, 50, stdin);
+  if (s[strlen(s) - 1] == '\n')
+    s[strlen(s) - 1] = '\0';
   printf("Enter a floating point number:\n");
-  scanf("%f", (*stc).floatingPoint);
+  scanf("%f", &f);
+
+  (*stc).character = c;
+  (*stc).integer = i;
+  (*stc).stringp = s;
+  (*stc).floatingPoint = f;
 }
